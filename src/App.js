d1bdcbd553a8323d4984cb04958c2547069d338e
lluper <leonardoluper5@gmail.com>
2020-07-02 22:35:20 -0300

    UPLOAD all files

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { Cards, Chart, CountryPicker } from './Components'
+import styles from './App.module.css'
+import { fetchData } from './api/'
+import Image from './images/image.png'
+function App() {
+  const [data, setData] = useState({})
+  const [country, setCountry] = useState('')
+
+  useEffect(() => {
+    const fetch = async () => {
+      const fetchedData = await fetchData();
+      setData(await fetchedData)
+      console.log(await fetchedData)
+    }
+    fetch()
+  }, [])
+
+  async function handleCountryChange(country){
+    const fetchedData = await fetchData(country);
+    setData(fetchedData)
+    setCountry(country)
+  }
+
+  return (
+    <div className={styles.container}>
+      <img classImage={styles.image} src={Image} />
+      <Cards data={data}/>
+      <CountryPicker handleCountryChange={handleCountryChange}/>
+      <Chart  data={data} country={country}/>
+    </div>
+  );
+}
+
+export default App;
